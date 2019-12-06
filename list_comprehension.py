1714b3c51fdf3ec93d20c5ed06179dbaef7f3e1a
Karunakar Venkatesh <KVenkatesh@oa.com.au>
2019-12-06 18:30:22 +1100

    If Condition Check in list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,11 +1,14 @@
-temps = [220, 230, 450, 789]
+temps = [220, 230, 450, 789, -9999]
 #list comprehension
-new_temps = [temp/10 for temp in temps]
+#new_temps = [temp/10 for temp in temps]
+
+new_temps = [temp / 10 for temp in temps if temp != -9999]
 
 #Older way of doing it
 """ new_temps = []
 for temp in temps:
-    value = temp/10
-    new_temps.append(value) """
+    if(temp != -9999):
+        value = temp/10
+        new_temps.append(value) """
 
 print(new_temps)
